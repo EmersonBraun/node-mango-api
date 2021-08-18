72bc3b16f277f4c7de9427059d396fbf7039ea76
EmersonBraun <emersonfbraun@gmail.com>
2021-08-18 20:14:45 -0300

    feat: ensure requests return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,10 @@
 import { Express } from 'express'
 import { bodyBarser } from '../middlewares/body-parser'
+import { contentType } from '../middlewares/content-type'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(bodyBarser)
+  app.use(cors)
+  app.use(contentType)
 }
